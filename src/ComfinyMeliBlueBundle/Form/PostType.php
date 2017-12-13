7f50161de202fe7fc77c93743a79b51b40d09624
mel1ssa <melissa.mekaoui@gmail.com>
2017-12-13 23:59:37 +0100

    create post form

--- src/ComfinyMeliBlueBundle/Form/PostType.php
+++ src/ComfinyMeliBlueBundle/Form/PostType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class TaskType extends AbstractType
+class PostType extends AbstractType
 {
 public function buildForm(FormBuilderInterface $builder, array $options)
 {
