0f6933d904749089fcb5e027db11e7816331acfc
stardisblue <stardisblue@users.noreply.github.com>
2017-12-30 16:36:04 +0100

    setting up trusted proxies

--- web/app.php
+++ web/app.php
@@ -15,6 +15,10 @@ if (PHP_VERSION_ID < 70000) {
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
+Request::setTrustedProxies(array($request->server->get('REMOTE_ADDR')));
+Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
+Request::setTrustedHeaderName(Request::HEADER_CLIENT_HOST, null);
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
